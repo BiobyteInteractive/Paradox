8427843e34d99fd68c51fd86baf1232cae35b7ed
pbentes <pedrogomes2000.3@gmail.com>
2024-04-09 14:10:09 +0100

    Added an hello window in the editor project

--- source/editor/src/main.c
+++ source/editor/src/main.c
@@ -1,5 +1,32 @@
-#include <stdio.h>
+#include "engine.h"
+#include <raylib.h>
+#include <flecs.h>
 
-int main() {
-    printf("Hello world from editor!");
+int main(void)
+{
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+
+    InitApplication("Paradox Game Engine - Editor");
+    MaximizeWindow();
+
+    ecs_world_t* ecs = ecs_init();
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ecs_progress(ecs, 0);
+
+        DrawFPS(10, 10);
+        ClearBackground((Color)
+        {
+            15, 15, 15, 255
+        });
+        EndDrawing();
+    }
+
+    ecs_quit(ecs);
+    CloseWindow();
+
+    return 0;
 }
