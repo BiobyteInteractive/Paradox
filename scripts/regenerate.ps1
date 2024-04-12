9c36f48b16e41e63bd8e441bb62f22942726c7a3
pbentes <pedrogomes2000.3@gmail.com>
2024-04-12 23:13:44 +0100

    Moving project deleted all C specific files

--- scripts/regenerate.ps1
+++
@@ -1,4 +0,0 @@
-cd ..;
-git clean -Xdf;
-.\vendor\windows\premake5\premake5.exe vs2022;
-cd .\scripts;
--- source/engine/src/core/application.h
+++
@@ -1,8 +0,0 @@
-#ifndef CORE_APPLICATION_H
-#define CORE_APPLICATION_H
-
-#include <raylib.h>
-
-void InitApplication(const char* title);
-
-#endif // !CORE_APPLICATION_H
